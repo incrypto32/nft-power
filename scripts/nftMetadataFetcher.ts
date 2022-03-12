196774f5ff6dab760485cdb81682c35fb1597b84
incrypto <vpkrishnanand@gmail.com>
2022-03-12 20:22:13 +0530

    changes

--- scripts/nftMetadataFetcher.ts
+++ scripts/nftMetadataFetcher.ts
@@ -7,29 +7,22 @@ async function main() {
   // const contract = (await ethers.getContractAt("Token", "0x5180db8F5c931aaE63c74266b211F580155ecac8")) as Token;
   // console.log(await contract.name());
 
-
-  let i = 2195;
+  let start = 2195;
+  let end = 3000;
   let list = [];
-  while (true) {
+
+  for (let i = start; i < end; i++) {
     try {
       console.log(`Getting metadata from URI ${`https://ipfs.io/ipfs/QmQUZz71VhXsg8DbeBB2q9TVHqqLNHwSJjY8EfSH7gJDSr/${i}.json`}`);
       const metadata = await axios.get(`https://ipfs.io/ipfs/QmQUZz71VhXsg8DbeBB2q9TVHqqLNHwSJjY8EfSH7gJDSr/${i}.json`);
       console.log(metadata.data);
       metadata.data.id = i;
       list.push(metadata.data);
-
-      i++;
     } catch (error) {
-      console.log("===================================")
-      console.log("===================================")
-      console.log("===================================")
-      console.log(error);
-      console.log("===================================")
-      console.log("===================================")
-      console.log("===================================")
       continue;
     }
   }
+
   fs.writeFileSync("./result.json", JSON.stringify(list));
 }
 
